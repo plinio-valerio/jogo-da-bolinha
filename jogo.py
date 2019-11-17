6af5fdc52ea3bc777a9b5588299b576cd9fcd88c
Plinio Valerio <plinio.valerio@protonmail.com>
2019-11-17 15:33:58 -0300

    Fixed collision physics

--- jogo.py
+++ jogo.py
@@ -35,6 +35,8 @@ n_obstaculos = 5
 class Body:
     obj_idx = 0
     def __init__(self, body, mass, radius, pos_x, pos_y, vel_x=.0, vel_y=.0, name=None, lives=3, kill=False):
+        assert mass >= .0
+        assert radius > .0
         Body.obj_idx += 1
         self.repr = 'Body_' + str(Body.obj_idx)
         if name is None:
@@ -59,13 +61,14 @@ class Body:
         self.body.move(delta[0], delta[1])
         np.copyto(self.pos, self.pos_0)
         np.copyto(self.vel, self.vel_0)
-    def update(self, dt=1):
+    def update(self, dt=1.0):
+        assert dt > .0
         delta = self.vel * dt
         self.pos += delta
         self.body.move(delta[0], delta[1])
-        for obst in enumerate(self.obstacles):
+        for obst in self.obstacles:
             if self.in_contact[obst.repr]:
-                collision_angle = obstacle.get_normal_angle(self)
+                collision_angle = obst.get_normal_angle(self)
                 if collision_angle is None:
                     self.in_contact[obst.repr] = False
                     obst.in_contact[self.repr] = False
@@ -76,7 +79,8 @@ class Body:
                     obst.n_collisions += 1
                 self.in_contact[obst.repr] = touched
                 obst.in_contact[self.repr] = touched
-    def collide(self, obstacle):
+    def collide(self, obstacle, verbose=False):
+        assert isinstance(obstacle, Body)
         collision_angle = obstacle.get_normal_angle(self)
         if collision_angle is None:
             return False
@@ -84,6 +88,17 @@ class Body:
         m_b = obstacle.mass
         vel_a = self.vel.copy()
         vel_b = obstacle.vel.copy()
+        if verbose:
+            print("EVENT: collision")
+            print("\tBody A:", self.name)
+            print("\tBody B:", obstacle.name)
+            print("\tCollision angle: %.4f" % collision_angle)
+            print("\tm_a = %.2f" % m_a)
+            print("\tm_b = %.2f" % m_b)
+            print("\tPre-collision velocity (A):", vel_a)
+            print("\tPre-collision velocity (B):", vel_b)
+            print("\t\tPre-collision total momentum:", m_a * vel_a + m_b * vel_b)
+            print("\t\tPre-collision total energy:", 0.5 * m_a * np.linalg.norm(vel_a)**2 + 0.5 * m_b * np.linalg.norm(vel_b)**2)
 
         # STEP 0: change reference frame so that obstacle has velocity 0
         #         and its post-collision vel_y is zero (i.e. rotate space so that the collision angle is 0)
@@ -114,23 +129,24 @@ class Body:
 
         # STEP 1: adopt coordinate system in which the x and y axes represent the post-collision velocities of bodies A and B, respectively;
         #         scale coordinate system (by the square root of the masses) so that conservation of energy yields a circumference.
-
         # Conservation of momentum:  m_a * x  + m_b * y  = m_a * vel_a_x
         # Conservation of energy:    m_a * x² + m_b * y² = m_a * vel_a_x²
         solution = np.array([vel_a[0], .0])  # pre-collision solution
 
-        matrix_1 = np.array([[1.0/math.sqrt(m_a), .0],
-                             [.0, 1.0/math.sqrt(m_b)]])
-        matrix_1_inv = np.array([[math.sqrt(m_a), .0],
-                                 [.0, math.sqrt(m_b)]])
+        matrix_1 = np.array([[math.sqrt(m_a),        .0      ],
+                             [     .0,         math.sqrt(m_b)]])
+        matrix_1_inv = np.array([[1.0/math.sqrt(m_a),           .0        ],
+                                 [       .0,            1.0/math.sqrt(m_b)]])
         solution = np.dot(matrix_1, solution)
 
         # STEP 2: rotate coordinate system so that conservation of momentum yields a line parallel to the x axis
-        line_angle = math.atan(-sqrt(m_a/m_b))
+        # Conservation of momentum:  ²m_a * x  + ²m_b * y  = m_a * vel_a_x
+        # Conservation of energy:    x² + y² = m_a * vel_a_x²
+        line_angle = math.atan(-math.sqrt(m_a/m_b))
         matrix_2 = np.array([[math.cos(-line_angle), -math.sin(-line_angle)],
-                             [math.sin(-line_angle),  math.sin(-line_angle)]])
+                             [math.sin(-line_angle),  math.cos(-line_angle)]])
         matrix_2_inv = np.array([[math.cos(line_angle), -math.sin(line_angle)],
-                                 [math.sin(line_angle),  math.sin(line_angle)]])
+                                 [math.sin(line_angle),  math.cos(line_angle)]])
         solution = np.dot(matrix_2, solution)
 
         # Under the new coordinate system, the 2 solutions to the conservation equations are symmetric wrt y axis
@@ -145,24 +161,33 @@ class Body:
         vel_b = np.dot(matrix_0_inv, vel_b)
         vel_a += translation_vector
         vel_b += translation_vector
+        if verbose:
+            print("\tPost-collision velocity (A):", vel_a)
+            print("\tPost-collision velocity (B):", vel_b)
+            print("\t\tPost-collision total momentum:", m_a * vel_a + m_b * vel_b)
+            print("\t\tPost-collision total energy:", 0.5 * m_a * np.linalg.norm(vel_a)**2 + 0.5 * m_b * np.linalg.norm(vel_b)**2)
+            print()
         self.vel = vel_a.copy()
         obstacle.vel = vel_b.copy()
         return True
     def within_collision_distance(self, obstacle):
-        distance = np.norm(self.pos - obstacle.pos)
+        assert isinstance(obstacle, Body)
+        distance = np.linalg.norm(self.pos - obstacle.pos)
         if distance > obstacle.radius + self.radius:
             return False
         return True
     def get_normal_angle(self, corpo):
         raise NotImplementedError
     def add_obstacle(self, body):
+        assert isinstance(body, Body)
         self.obstacles.add(body)
         body.obstacles.add(self)
         self.in_contact[body.repr] = False
         body.in_contact[self.repr] = False
-    def on_collision(self, projetil):
+    def on_collision(self, projectile):
+        assert isinstance(projectile, Body)
         if self.kill:
-            projetil.vidas -= 1
+            projetil.lives -= 1
             projetil.on_death()
     def on_death(self):
         self.reset()
@@ -188,7 +213,11 @@ class Ball(Body):
             mass = math.pi * radius**2
         Body.__init__(self, body=Circle(center, radius), mass=mass, radius=radius, pos_x=center.getX(), pos_y=center.getY(), vel_x=vel_x, vel_y=vel_y, name=name, lives=lives, kill=kill)
     def get_normal_angle(self, projectile):
-        return math.atan2(projectile.pos_y - self.pos_y, projectile.pos_x - self.pos_x)
+        assert isinstance(projectile, Body)
+        if not self.within_collision_distance(projectile):
+            return None
+        relative_position = projectile.pos - self.pos
+        return math.atan2(relative_position[1], relative_position[0])
 
 class RegularPolygon(Body):
     obj_idx = 0
@@ -202,28 +231,31 @@ class RegularPolygon(Body):
                                center.getY() + radius * math.sin(self.angle + i * math.tau / self.n_edges)) for i in range(self.n_edges)]
         poly = Polygon(self.vertices)
         Body.__init__(self, body=poly, mass=math.inf, radius=radius, pos_x=center.getX(), pos_y=center.getY(), name=name, lives=lives, kill=kill)
+    def update(self):
+        pass
     def get_normal_angle(self, projectile, verbose=False):
-        if not self.is_in_collision_zone(projectile):
+        assert isinstance(projectile, Body)
+        if not self.within_collision_distance(projectile):
             return None
-        angulo_entre_vertices = math.tau / self.n_lados
-        angulo = math.atan2(projetil.pos_y - self.pos_y, projetil.pos_x - self.pos_x) - self.angle
+        angulo_entre_vertices = math.tau / self.n_edges
+        angulo = math.atan2(projectile.pos[1] - self.pos[1], projectile.pos[0] - self.pos[0]) - self.angle
         idx_v = int(angulo // angulo_entre_vertices)
         p1 = self.vertices[idx_v]
-        p2 = self.vertices[(idx_v - 1) % self.n_lados]
+        p2 = self.vertices[(idx_v - 1) % self.n_edges]
 
         # parametros da equacao da reta (aresta do poligono)
         a = p1.getY() - p2.getY()
         b = p2.getX() - p1.getX()
-        distancia_bola_para_aresta = math.fabs(a * (projectile.pos_x - p1.getX()) + b * (projectile.pos_y - p1.getY())) / math.sqrt(a**2 + b**2)
+        distancia_bola_para_aresta = math.fabs(a * (projectile.pos[0] - p1.getX()) + b * (projectile.pos[1] - p1.getY())) / math.sqrt(a**2 + b**2)
 
         if verbose:
             print("Evento: possivel colisao")
-            print("\tProjetil:", projectile.name, "\t== (%d, %d)" % (projectile.pos_x, projectile.pos_y))
+            print("\tProjetil:", projectile.name, "\t== (%d, %d)" % (projectile.pos[0], projectile.pos[1]))
             print("\tObstaculo:", self.name)
             print("\tAngulo do poligono: %d" % int(self.angle / math.tau * 360))
             print("\tAngulo do projetil: %d" % int(angulo / math.tau * 360))
             print("\tVertices:  ", [(int(v.getX()), int(v.getY())) for v in self.vertices])
-            print("\tDistancias:", ["  %.2f" % math.sqrt((projectile.pos_x - v.getX())**2 + (projectile.pos_y - v.getY())**2) for v in self.vertices])
+            print("\tDistancias:", ["  %.2f" % math.sqrt((projectile.pos[0] - v.getX())**2 + (projectile.pos[1] - v.getY())**2) for v in self.vertices])
             print("\tp1: (%d, %d)" % (p1.getX(), p1.getY()))
             print("\tp2: (%d, %d)" % (p2.getX(), p2.getY()))
             print("\ta = %.2f" % a)
@@ -234,7 +266,7 @@ class RegularPolygon(Body):
             return None
         # return math.atan(-a/b) + math.tau/4
         return angulo // angulo_entre_vertices * angulo_entre_vertices + self.angle + angulo_entre_vertices / 2
-        # angulo_entre_vertices = math.tau / self.n_lados
+        # angulo_entre_vertices = math.tau / self.n_edges
         # angulo = math.atan2(y, x) - self.angle
         # return angulo // angulo_entre_vertices * angulo_entre_vertices + angulo_entre_vertices / 2
 
@@ -251,10 +283,16 @@ class Bar(Body):
         radius = math.sqrt((p2.getX() - p1.getX())**2 + (p2.getY() - p1.getY())**2) / 2
         self.normal_angle = math.tau / 4
         Body.__init__(self, body=Rectangle(p1, p2), mass=math.inf, radius=radius, pos_x=pos_x, pos_y=pos_y, name=name)
+    def update(self, dt=1.0):
+        assert dt > .0
+        delta = self.vel * dt
+        self.pos += delta
+        self.body.move(delta[0], delta[1])
     def get_normal_angle(self, projectile):
-        if not self.is_in_collision_zone(projectile):
+        assert isinstance(projectile, Body)
+        if not self.within_collision_distance(projectile):
             return None
-        ball_to_edge_distance = math.fabs(projectile.pos_y - self.pos_y + self.height / 2)
+        ball_to_edge_distance = math.fabs(projectile.pos[1] - self.pos[1] + self.height / 2)
         if ball_to_edge_distance > projectile.radius:
             return None
         return self.normal_angle
@@ -270,20 +308,25 @@ class Wall(Body):
         radius = math.sqrt((p2.getX() - p1.getX())**2 + (p2.getY() - p1.getY())**2) / 2
         self.normal_angle = math.atan2(p2.getY() - p1.getY(), p2.getX() - p1.getX()) + math.tau / 4
         Body.__init__(self, body=Line(p1, p2), mass=math.inf, radius=radius, pos_x=pos_x, pos_y=pos_y, name=name, lives=lives, kill=kill)
+    def update(self):
+        pass
     def get_normal_angle(self, projectile):
+        assert isinstance(projectile, Body)
         p1 = self.body.getP1()
         p2 = self.body.getP2()
         # parametros da equacao da reta (aresta do poligono)
         a = p1.getY() - p2.getY()
         b = p2.getX() - p1.getX()
-        distancia_bola_para_aresta = math.fabs(a * (projectile.pos_x - p1.getX()) + b * (projectile.pos_y - p1.getY())) / math.sqrt(a**2 + b**2)
+        distancia_bola_para_aresta = math.fabs(a * (projectile.pos[0] - p1.getX()) + b * (projectile.pos[1] - p1.getY())) / math.sqrt(a**2 + b**2)
         if distancia_bola_para_aresta > projectile.radius + self.width:
+            return None
+        if not self.within_collision_distance(projectile):
             return None
         return self.normal_angle
     def on_collision(self, projetil):
         if self.kill:
-            projetil.vidas -= 1
-            if projetil.vidas <= 0:
+            projetil.lives -= 1
+            if projetil.lives <= 0:
                 return
             projetil.reset()
             time.sleep(1)
@@ -330,7 +373,7 @@ barra.setOutline(outline_barra)
 barra.setWidth(2)
 
 # bolinha
-initial_angle = (2*random.random() - 1) * math.tau / 4 + math.tau / 4
+initial_angle = (2*random.random() - 1) * math.tau / 8 + math.tau / 4
 bola = Ball(Point(width/2, height/2), raio_bola, vel_x=vel_inicial*math.cos(initial_angle), vel_y=vel_inicial*math.sin(initial_angle), lives=vidas_iniciais, name="Bolinha")
 bola.setFill(fill_bola)
 bola.setOutline(outline_bola)
@@ -401,21 +444,21 @@ while True:
     while True:
         # movimento horizontal da barra pelas setas direita/esquerda
         tecla = win.checkKey()
-        if (tecla == "Right" or tecla == 'd') and barra.pos_x < width - dr - comprimento_barra/2:
+        if (tecla == "Right" or tecla == 'd') and barra.pos[0] < width - dr - comprimento_barra/2:
             barra.vel[0] = velocidade_barra
-        if (tecla == "Left" or tecla == 'a') and barra.pos_x > dl + comprimento_barra/2:
+        if (tecla == "Left" or tecla == 'a') and barra.pos[0] > dl + comprimento_barra/2:
             barra.vel[0] = -velocidade_barra
-        if (tecla == '') or barra.pos_x >= width - dr - comprimento_barra/2 and barra.vel_x > 0 or barra.pos_x <= dl + comprimento_barra/2 and barra.vel_x < 0:
+        if (tecla == '') or barra.pos[0] >= width - dr - comprimento_barra/2 and barra.vel[0] > 0 or barra.pos[0] <= dl + comprimento_barra/2 and barra.vel[0] < 0:
             barra.vel[0] = 0
         if tecla == "Escape":  # sai do jogo
-            bola.vidas = -1
+            bola.lives = -1
 
         bola.update()
         barra.update()
         for obst in obstaculos:
             obst.update()
 
-        if bola.vidas <= 0:
+        if bola.lives <= 0:
             fim_txt = Text(Point(width / 2, height / 2), "GAME\nOVER")
             fim_txt.setStyle('bold')
             fim_txt.setSize(32)
@@ -441,7 +484,7 @@ while True:
 
         if t_steps % 5 == 0:
             info_txt.undraw()
-            info_txt.setText("Colisões: " + str(bola.n_collisions) + "\t\tVidas: " + str(bola.lives) + "\t\tVelocidade: %.2f" % np.norm(bola.vel) + "\t\tTempo: %.1f" % t)
+            info_txt.setText("Colisões: " + str(bola.n_collisions) + "\t\tVidas: " + str(bola.lives) + "\t\tVelocidade: %.2f" % np.linalg.norm(bola.vel) + "\t\tTempo: %.1f" % t)
             info_txt.draw(win)
 
         time.sleep(dt)
