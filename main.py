66092614887b0e9097d4baf7ab2191dcf259f49c
Joaoobarreto <57297646+Joaoobarreto@users.noreply.github.com>
2019-11-20 02:00:46 -0300

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,6 @@ velocidade_barra = 1000.0  # velocidade horizontal da barra (pixels/segundo) a c
 fill_barra = color_rgb(100, 10, 10)  # cor de preenchimento da barra
 outline_barra = color_rgb(255, 255, 0)  # cor do contorno da barra
 
-
 pontos_por_fase = 3  # quantidade de pontos necessaria para acelerar a bolinha
 vel_inicial = 400.0  # velocidade inicial da bolinha, em pixels/segundo
 dt = 0.020  # intervalo de tempo entre dois frames do jogo, em segundos
@@ -54,52 +53,78 @@ linhaDireita = Wall(Point(width - dr + tilt, dd), Point(width - dr - tilt, heigh
 linhaDireita.setWidth(10)
 linhaDireita.setFill(color_rgb(10, 100, 10))
 
-espaco_branco = Rectangle(Point(0, dd - 2), Point(width, 0))
-espaco_branco.setFill('white')
-espaco_branco.setOutline('white')
+# espaco_branco = Rectangle(Point(0, dd - 2), Point(width, 0))
+# espaco_branco.setFill('white')
+# espaco_branco.setOutline('white')
 
 # texto
-info_txt = Text(Point(width / 2, 25), '')
+info_txt = Text(Point(width / 2, 20), '')
 info_txt.setSize(14)
+info_txt.setTextColor('yellow')
 
 # barra
-barra = Bar(Point(width/2 - comprimento_barra/2, dd + db + espessura_barra/2),
-            Point(width/2 + comprimento_barra/2, dd + db - espessura_barra/2),
+barra = Bar(Point(width / 2 - comprimento_barra / 2, dd + db + espessura_barra / 2),
+            Point(width / 2 + comprimento_barra / 2, dd + db - espessura_barra / 2),
             velocidade_barra)
 barra.setFill(fill_barra)
 barra.setOutline(outline_barra)
 barra.setWidth(2)
 
 # bolinha
-initial_angle = (2*random.random() - 1) * math.tau / 8 + math.tau / 4
-bola = Ball(Point(width/2, height/2), raio_bola, vel_x=vel_inicial*math.cos(initial_angle), vel_y=vel_inicial*math.sin(initial_angle), lives=vidas_iniciais, name="Bolinha")
+initial_angle = (2 * random.random() - 1) * math.tau / 8 + math.tau / 4
+bola = Ball(Point(width / 2, height / 2), raio_bola, vel_x=vel_inicial * math.cos(initial_angle),
+            vel_y=vel_inicial * math.sin(initial_angle), lives=vidas_iniciais, name="Bolinha")
 bola.setFill(fill_bola)
 bola.setOutline(outline_bola)
 bola.setWidth(2)
 
+
 ################################################################################
 ####                                  JOGO                                  ####
 ################################################################################
+imagem = Image(Point(width / 2, height / 2), "fundojogo2.png")
+imagem.draw(win)
+def texto(texto):
+    texto = Text(Point(width / 2, height / 2), texto)
+    texto.setStyle('bold')
+    texto.setTextColor('yellow')
+    texto.setSize(26)
+    texto.draw(win)
+    if win.getKey() == "Right" or win.getKey() == "Return":
+        texto.undraw()
+    return
 
-# menu
-while True:
+
+def historia():
     win.setBackground('black')
-    inicio_txt = Text(Point(width / 2, height / 2), "Aperte qualquer tecla para iniciar\nPressione ESC para sair do jogo")
-    inicio_txt.setStyle('bold')
-    inicio_txt.setTextColor('white')
-    inicio_txt.setSize(32)
-    inicio_txt.draw(win)
-    tecla = win.getKey()
-    if (tecla == 'Escape'):
-        break
+    texto("No ano de 2050, um gigantesco astro \npassa perto do sistema solar.\n"
+          " Sua massa é tão grande que tira os\n planetas de sua órbita e, ao arrastá-los,\n"
+          " aprisiona-os numa espécie de campo\n de força. ")
+    if win.getKey() == "Right" or win.getKey() == "Return":
 
-    # comeca jogo
+        texto("Não é só isso. As características \nfísico-químicas dos planetas são alteradas,\n"
+              " de modo a torná-los elásticos e,\n em alguns casos, divisíveis.Os planetas,\n"
+              "desgovernados, chocam-se aleatoriamente.\n"
+              " Pequena, a Terra está a ponto de\n ser expelida do campo de força e\n"
+              "perder-se para sempre no espaço infinito.  ")
+        if win.getKey() == "Right" or win.getKey() == "Return":
+
+            texto("Felizmente, um grupo de cientistas \nda computação havia previsto a catástrofe.\n"
+                  " Eles conseguiram escapar do desastre,\n juntamente com alguns habitantes da Terra.\n"
+                  " Você é um dos que foram salvos.")
+            if win.getKey() == "Right" or win.getKey() == "Return":
+
+                texto("Agora, a sua missão é evitar que o \npequeno planeta azul se perca no Universo,\n"
+                      " até que a situação se normalize, \nna esperança de um dia regressar a sua terra natal. ")
+historia()
+def Iniciar():
+    inicio_txt.undraw()
     win.setBackground('white')
     linhaSuperior.draw(win)
     linhaInferior.draw(win)
     linhaEsquerda.draw(win)
     linhaDireita.draw(win)
-    espaco_branco.draw(win)
+    # espaco_branco.draw(win)
     barra.reset()
     barra.add_obstacle(linhaEsquerda)
     barra.add_obstacle(linhaDireita)
@@ -112,8 +137,9 @@ while True:
     obstaculos = []
     for i in range(n_obstaculos):  # cria obstaculos
         radius = random.random() * 30 + 30
-        center = Point((width - dr - dl - 3*radius) * random.random() + dl + 1.5*radius,
-                       (height - du - dd - db - 3*radius) / 3 * random.random() + 2 * (height - du - dd - db - 3*radius) / 3 + dd + db + 1.5 * radius)
+        center = Point((width - dr - dl - 3 * radius) * random.random() + dl + 1.5 * radius,
+                       (height - du - dd - db - 3 * radius) / 3 * random.random() + 2 * (
+                                   height - du - dd - db - 3 * radius) / 3 + dd + db + 1.5 * radius)
         if random.random() < 0.5:  # probabilidade de obstaculo ser um circulo
             vel_x = random.gauss(0, 100)
             vel_y = random.gauss(0, 50)
@@ -146,11 +172,12 @@ while True:
     while True:
         # movimento horizontal da barra pelas setas direita/esquerda
         tecla = win.checkKey()
-        if (tecla == "Right" or tecla == 'd') and barra.pos[0] < width - dr - comprimento_barra/2:
+        if (tecla == "Right" or tecla == 'd') and barra.pos[0] < width - dr - comprimento_barra / 2:
             barra.vel[0] = velocidade_barra
-        if (tecla == "Left" or tecla == 'a') and barra.pos[0] > dl + comprimento_barra/2:
+        if (tecla == "Left" or tecla == 'a') and barra.pos[0] > dl + comprimento_barra / 2:
             barra.vel[0] = -velocidade_barra
-        if (tecla == '') or barra.pos[0] >= width - dr - comprimento_barra/2 and barra.vel[0] > 0 or barra.pos[0] <= dl + comprimento_barra/2 and barra.vel[0] < 0:
+        if (tecla == '') or barra.pos[0] >= width - dr - comprimento_barra / 2 and barra.vel[0] > 0 or barra.pos[
+            0] <= dl + comprimento_barra / 2 and barra.vel[0] < 0:
             barra.vel[0] = 0
         if tecla == "Escape":  # sai do jogo
             bola.lives = -1
@@ -163,6 +190,7 @@ while True:
         if bola.lives <= 0:
             fim_txt = Text(Point(width / 2, height / 2), "GAME\nOVER")
             fim_txt.setStyle('bold')
+            fim_txt.setTextColor("yellow")
             fim_txt.setSize(32)
             fim_txt.draw(win)
             time.sleep(2)
@@ -179,18 +207,39 @@ while True:
             bola.obstacles = set()
             bola.in_contact = {}
             bola.n_collisions = 0
-            espaco_branco.undraw()
+            # espaco_branco.undraw()
             bola.undraw()
             barra.undraw()
             break
 
         if t_steps % 5 == 0:
             info_txt.undraw()
-            info_txt.setText("Colisões: " + str(bola.n_collisions) + "\t\tVidas: " + str(bola.lives) + "\t\tVelocidade: %.2f" % np.linalg.norm(bola.vel) + "\t\tTempo: %.1f" % t)
+            info_txt.setText("Colisões: " + str(bola.n_collisions) + "\t\tVidas: " + str(
+                bola.lives) + "\t\tVelocidade: %.2f" % np.linalg.norm(bola.vel) + "\t\tTempo: %.1f" % t)
             info_txt.draw(win)
 
         time.sleep(dt)
         t_steps += 1
         t += dt
 
+
+
+
+# menu
+while True:
+    win.setBackground('black')
+    iniciar = 'INICIAR'
+    ranking = 'RANKING'
+    config = 'CONFIGURAÇÕES'
+    opcoes = [iniciar, ranking, config]
+    inicio_txt = Text(Point(width / 2, height / 2), "{}\n{}\n{}\n".format(iniciar, ranking, config))
+    inicio_txt.setStyle('bold')
+    inicio_txt.setTextColor('white')
+    inicio_txt.setSize(32)
+    inicio_txt.draw(win)
+
+    tecla = win.getKey()
+    if (tecla == 'Escape'):
+        break
+    Iniciar()
 win.close()
